76e91e9462f41777c106e78cadd3efd786dd0eee
jordan_samson <samson@flightsafety.org>
2024-07-02 08:56:49 +1000

    nice

--- gen_data/src/gen_common.rs
+++ gen_data/src/gen_common.rs
@@ -11,3 +11,14 @@ pub fn get_col_type(col_type: &str) -> ColTypesEnum {
         _ => ColTypesEnum::VARCHAR,
     }
 }
+
+//Add structs and their implementations to each of the created files
+pub fn get_data_type(col_type: ColTypesEnum) -> String {
+    match col_type {
+        ColTypesEnum::VARCHAR => "String".to_string(),
+        ColTypesEnum::INTEGER => "i32".to_string(),
+        ColTypesEnum::FLOAT => "f64".to_string(),
+        ColTypesEnum::UUID => "uuid::uuid".to_string(),
+        ColTypesEnum::BOOLEAN => "bool".to_string(),
+    }
+}
--- gen_data/src/scaffold.rs
+++ gen_data/src/scaffold.rs
@@ -1,30 +1,49 @@
-use std::fs::File;
+#![allow(dead_code)]
 use std::fs;
-use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
+use std::{fs::File, io::Write};
 
-use crate::{gen_common::get_col_type, structs::{DataColumn, DataTable}};
+use crate::{
+    gen_common::get_col_type,
+    structs::{DataColumn, DataTable},
+};
 
-static FILE_LOCATION: &str = "init_db.sql";
+//Static variables
 static CREATE_TABLE: &str = "CREATE TABLE";
 static EXISTS: &str = "EXISTS";
 
-pub fn read_files() {
-    println!("---- Reading SQL Schema from: {} ----", FILE_LOCATION);
+//Eventually these will come from the args when running the program.
+static SQL_FILE: &str = "init_db.sql";
+static ATTRIBUTE_FILE: &str = "Templates/attributes.txt";
+static STRUCT_TEMPLATE_FILE: &str = "Templates/struct_template.txt";
 
-    let contents = fs::read_to_string(FILE_LOCATION).expect("There was an error reading the file");
+pub fn read_files() {
+    println!("---- Reading SQL Schema from: {} ----", SQL_FILE);
+
+    let contents = fs::read_to_string(SQL_FILE).expect("Error reading the SQL file.");
     println!("---- Reading file from database schema ----");
 
-    let tables = parse_db_file(&contents);
+    let tables = read_db_init_file(&contents);
 
-    write_to_rust_file(tables);
+    let create_file_result = write_to_rust_file(&tables);
+    match create_file_result {
+        Ok(_) => println!("---- Successfully created Rust file: {} ----", SQL_FILE),
+        Err(e) => println!("---- Error creating Rust file: {} ----", e),
+    }
+
+    let create_file_result = generate_mod_file(&tables);
+    match create_file_result {
+        Ok(_) => println!("---- Successfully created Struct Mod file ----"),
+        Err(e) => println!("---- Error creating Mod file: {} ----", e),
+    }
 }
 
-//not sure if its reall the best idea to read the entire database file out as a string.
-//Is there any better way to do it????
-fn parse_db_file(sql_file: &str) -> Vec<DataTable> {
+///---------------------------------------------------------------------------------------------------------------
+/// Reads the database file and creates the rust structs as defined by the database.
+///---------------------------------------------------------------------------------------------------------------
+fn read_db_init_file(sql_file: &str) -> Vec<DataTable> {
     //-----Tables-----
     let mut tables: Vec<DataTable> = Vec::new();
-    let raw_tables: Vec<&str> = sql_file.split(EXISTS).collect(); //Gets the table name. 
+    let raw_tables: Vec<&str> = sql_file.split(EXISTS).collect(); //Gets the table name.
     let count: usize = raw_tables.len();
     println!("Table Count: {}", count);
 
@@ -46,18 +65,18 @@ fn parse_db_file(sql_file: &str) -> Vec<DataTable> {
 
         for col in table_cols {
             // println!("col Details: {}", col.trim_start());
-            let col_details: Vec<&str> = col.trim_start().split_ascii_whitespace().collect();
+            let col_details_sql: Vec<&str> = col.trim_start().split_ascii_whitespace().collect();
 
-            if col_details.len() < 2 {
+            if col_details_sql.len() < 2 {
                 panic!(
                     "Column in {} is missing type. Please fix the table. ",
                     table_name
                 )
             }
 
-            let col_type = get_col_type(col_details[1]);
+            let col_type = get_col_type(col_details_sql[1]);
 
-            columns.push(DataColumn::new(col_type, col_details[1].to_string()))
+            columns.push(DataColumn::new(col_type, col_details_sql[1].to_string()))
         }
         tables.push(DataTable::new(table_name.to_owned(), columns))
     }
@@ -65,16 +84,49 @@ fn parse_db_file(sql_file: &str) -> Vec<DataTable> {
     tables
 }
 
-fn write_to_rust_file(sql_tables: Vec<DataTable>) {
-    let path = "TestModels/";
+///---------------------------------------------------------------------------------------------------------------
+/// Writes the structs as the rust file.
+///---------------------------------------------------------------------------------------------------------------
+fn write_to_rust_file(sql_tables: &Vec<DataTable>) -> std::io::Result<()> {
+    let path = "DataStructs/";
 
-    sql_tables.par_iter().for_each(|x: &DataTable| {
-        let file_path: String = path.to_owned() + &x.table_name + ".rs";
-        let mut _file = File::create(file_path);
-        
-        //write the contents to the file.
-    });
+    for table in sql_tables {
+        let file_path: String = path.to_owned() + &table.table_name + ".rs";
+        let mut file = File::create_new(file_path)?;
+        let struct_details = generate_struct_data(&table);
+        file.write(struct_details.as_bytes())?;
+    }
+    Ok(())
 }
 
+///---------------------------------------------------------------------------------------------------------------
+/// Generates the struct data for the rust file.
+///--------------------------------------------------------------------------------------------------------------
+fn generate_struct_data(table: &DataTable) -> String {
+    let mut struct_data: String = "".to_string();
+    struct_data.push_str(&format!(
+        "{}\\n
+        pub struct {} {{\n
+        }}",
+        add_template_data(),
+        table.table_name
+    ));
+    struct_data
+}
 
+fn add_template_data() -> String {
+    fs::read_to_string(ATTRIBUTE_FILE).expect("There was an error reading the template file.")
+}
 
+///---------------------------------------------------------------------------------------------------------------
+/// Generates the mod file for the rust file.
+/// --------------------------------------------------------------------------------------------------------------
+fn generate_mod_file(sql_tables: &Vec<DataTable>) -> std::io::Result<()> {
+    let path = "DataStructs/".to_owned();
+    let mut file = File::create_new(path + "mod.rs")?;
+
+    for table in sql_tables {
+        file.write(format!("pub mod {};", table.table_name).as_bytes())?;
+    }
+    Ok(())
+}
